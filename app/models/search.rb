56c2bde8f958208804473ff53c54c54751a3be31
Jonathan Pulsifer <jonathan@pulsifer.ca>
2018-10-03 14:12:06 -0400

    Revert "Add inbox:true search prefix (#947)"
    This reverts commit 9025b8e8bbe53b81fa8e7b853993a011e13f475e.

--- app/models/search.rb
+++ app/models/search.rb
@@ -20,7 +20,6 @@ class Search
     res = res.assigned(assignee) if assignee.present?
     res = res.starred(starred) unless starred.nil?
     res = res.archived(archived) unless archived.nil?
-    res = res.archived(!inbox) unless inbox.nil?
     res = res.unread(unread) unless unread.nil?
     res = res.bot_author unless bot_author.nil?
     res = res.unlabelled unless unlabelled.nil?
@@ -119,10 +118,6 @@ class Search
 
   def starred
     boolean_prefix(:starred)
-  end
-
-  def inbox
-    boolean_prefix(:inbox)
   end
 
   def archived
