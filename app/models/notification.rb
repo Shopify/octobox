f949e7e99bae80ad4ef42ba26189632624f40d96
Jonathan Pulsifer <jonathan@pulsifer.ca>
2018-10-03 14:12:06 -0400

    Revert "Addresses #811 Store muted_at and allow search filtering by muted:tru… (#923)"
    This reverts commit c2d06c59663b0455136a5a02c74249bcbfae9f96.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -25,8 +25,6 @@ class Notification < ApplicationRecord
   belongs_to :repository, foreign_key: :repository_full_name, primary_key: :full_name, optional: true
   has_many :labels, through: :subject
 
-  scope :unmuted,  -> { where("muted_at IS NULL") }
-  scope :muted,    -> { where("muted_at IS NOT NULL") }
   scope :inbox,    -> { where(archived: false) }
   scope :archived, ->(value = true) { where(archived: value) }
   scope :newest,   -> { order('notifications.updated_at DESC') }
@@ -119,7 +117,7 @@ class Notification < ApplicationRecord
       end
     end
 
-    where(id: notifications.map(&:id)).update_all(archived: true, unread: false, muted_at: Time.current)
+    where(id: notifications.map(&:id)).update_all(archived: true, unread: false)
   end
 
   def expanded_subject_url
