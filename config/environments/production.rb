2c5384278ca391e9b6c6c9138abffd65379da1ea
Jonathan Pulsifer <jonathan@pulsifer.ca>
2018-10-03 14:12:06 -0400

    Revert "Add redis and faraday probes to skylight config (#941)"
    This reverts commit fa01c2359a89ace3bd5f8a7be886f8d4ebe7d65f.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.lograge.enabled = true
-
-  config.skylight.probes += %w(redis faraday) if ENV['SKYLIGHT_AUTHENTICATION'].present?
 end
