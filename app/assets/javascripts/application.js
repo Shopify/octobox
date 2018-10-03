0e1067de299f36731b5f7a9f12e60af17a133c63
Jonathan Pulsifer <jonathan@pulsifer.ca>
2018-10-03 14:12:06 -0400

    Revert "Add feature list to homepage (#869)"
    This reverts commit 1e40a2661bf68dfb3cf7bbd3c961581a963a0b00.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -146,13 +146,15 @@ document.addEventListener("turbolinks:load", function() {
       checkAll($(".js-select_all").prop('checked'))
     })
 
-    enableTooltips();
+    if(!('ontouchstart' in window))
+    {
+      $('[data-toggle="tooltip"]').tooltip()
+    }
 
-    updateFavicon();
+    updateFavicon()
   }
 });
 
-$(document).ready(enableTooltips);
 
 document.addEventListener("turbolinks:before-cache", function() {
   $('td.js-current').removeClass("current js-current");
@@ -161,13 +163,6 @@ document.addEventListener("turbolinks:before-cache", function() {
 $(document).on('click', '[data-toggle="offcanvas"]', function () {
   $('.flex-content').toggleClass('active')
 });
-
-function enableTooltips() {
-  if(!('ontouchstart' in window))
-  {
-    $('[data-toggle="tooltip"]').tooltip()
-  }
-}
 
 function enableKeyboardShortcuts() {
   // Add shortcut events only once
