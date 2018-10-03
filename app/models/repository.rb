207f806214e2002aaad00b01b5edc2c5fedef3e0
Jonathan Pulsifer <jonathan@pulsifer.ca>
2018-10-03 14:12:06 -0400

    Revert " Store repository_full_name on Subject and setup proper relations with Repository (#854)"
    This reverts commit 5760f86de0a641ff7f4e36a01080c72a5a3d1ec9.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,7 +1,5 @@
 class Repository < ApplicationRecord
   has_many :notifications, foreign_key: :repository_full_name, primary_key: :full_name
-  has_many :users, -> { distinct }, through: :notifications
-  has_many :subjects, foreign_key: :repository_full_name, primary_key: :full_name
   belongs_to :app_installation
 
   validates :full_name, presence: true, uniqueness: true
@@ -9,5 +7,9 @@ class Repository < ApplicationRecord
 
   def github_app_installed?
     app_installation_id.present?
+  end
+
+  def subjects
+    Subject.repository(full_name)
   end
 end
--- db/migrate/20180830145149_add_repository_full_name_to_subjects.rb
+++
@@ -1,5 +0,0 @@
-class AddRepositoryFullNameToSubjects < ActiveRecord::Migration[5.2]
-  def change
-    add_column :subjects, :repository_full_name, :string
-  end
-end
