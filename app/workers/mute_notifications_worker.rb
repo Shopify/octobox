3ef81cbce15fdac3484d88801ce40db41afa3963
Jonathan Pulsifer <jonathan@pulsifer.ca>
2018-10-03 14:12:06 -0400

    Revert "Mute notifications on github in the background if sidekiq configured (#971)"
    This reverts commit 6e7f48fe636c8cf93827be3b63fd38bb2df00c23.

--- app/workers/mute_notifications_worker.rb
+++
@@ -1,9 +0,0 @@
-class MuteNotificationsWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: :sync_notifications, unique: :until_and_while_executing
-
-  def perform(user_id, notification_ids)
-    user = User.find_by_id(user_id)
-    Notification.mute_on_github(user, notification_ids) if user
-  end
-end
